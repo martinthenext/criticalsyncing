bf3af842facd8ba8f22b3f24041c74bebe7a1fa3
Martin Davtyan <martin.davtyan@gmail.com>
2014-10-11 05:02:26 +0400

    view, admin and template

--- criticalsyncing/core/views.py
+++ criticalsyncing/core/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class Index(View):
+    template_name = "index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        pass
