45c9c67cfd57d842d1f2af0eb101a23122c3537a
martinthenext <martin.davtyan@gmail.com>
2014-10-11 05:15:41 +0200

    Useful comment

--- criticalsyncing/core/logic.py
+++ criticalsyncing/core/logic.py
@@ -1,2 +1,10 @@
+"""
+
+If the article is from Western media and mentions some other country, find a source for that country
+	and search for the similar article on that source
+Otherwize, look at the source of the article and find an opposite source, 
+	and search for the similar article on that source
+	
+"""
 def get_matching_url(input_url):
 	return u"http://ya.ru"
