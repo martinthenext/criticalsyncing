97f303be8bcd037d214b8c0783b7b38099870637
martinthenext <martin.davtyan@gmail.com>
2014-10-11 15:39:58 +0200

    Start on working on vectorizing Article objects from db

--- criticalsyncing/core/fetch_articles.py
+++
@@ -1,6 +0,0 @@
-from models import Article, Source
-
-
-if __name__ == "__main__":
-    for s in Source.objects.all():
-        Article.objects.download_from(s)
--- criticalsyncing/core/views.py
+++ criticalsyncing/core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 import json
 from logic import get_matching_url
+from models import Cache
 
 class Index(View):
     template_name = "index.html"
@@ -29,6 +30,11 @@ class Index(View):
         input_url = request.POST.get("url")
         if not input_url:
             return self.error(request, "have no url")
-        output_url = get_matching_url(input_url)
+        cached_output = Cache.objects.filter(input_url=input_url)
+        if cached_output:
+        	 output_url = cached_output[0].output_url
+        else:
+        	output_url = get_matching_url(input_url)
+        	Cache.objects.create(input_url=input_url, output_url=output_url)
         # get new url
         return self.redirect(request, output_url)
